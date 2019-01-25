07fe4087690a0f482494506a73051deca45b4954
Jordan <jordanhaddadi@gmail.com>
2019-01-25 11:24:01 -0500

    cat and dog endpoints created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
+// const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
@@ -22,6 +22,35 @@ app.use(
   })
 );
 
+let catData = {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}
+
+let dogData = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}
+
+app.get('/api/cat', (req, res) => {
+    res.status(200).json(catData)
+});
+
+app.get('/api/dog', (req, res) => {
+    res.status(200).json(dogData)
+});
+
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
@@ -34,7 +63,6 @@ function runServer(port = PORT) {
 }
 
 if (require.main === module) {
-  dbConnect();
   runServer();
 }
 
